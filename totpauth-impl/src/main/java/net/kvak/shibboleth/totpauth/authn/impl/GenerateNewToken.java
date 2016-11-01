b80a6455ac12df4daf0ee1947cc727cddd933627
M. Adam Kendall <michael.kendall@gsa.gov>
2016-11-01 16:34:27 -0400

    Allow changing the issuer name for authenticator apps

--- totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/GenerateNewToken.java
+++ totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/GenerateNewToken.java
@@ -20,34 +20,43 @@ import net.shibboleth.utilities.java.support.primitive.StringSupport;
 
 @SuppressWarnings("rawtypes")
 public class GenerateNewToken extends AbstractProfileAction {
-	
+
 	/** Class logger. */
 	@Nonnull
 	private final Logger log = LoggerFactory.getLogger(GenerateNewToken.class);
-	
+
 	TokenUserContext tokenCtx;
-	
+
 	UsernamePasswordContext upCtx;
-	
+
 	/** Google Authenticator **/
 	@Nonnull
 	@NotEmpty
 	private GoogleAuthenticator gAuth;
-	
+
+	/** Issuer name for Authenticator **/
+	@Nonnull
+	@NotEmpty
+	private String gAuthIssuerName;
+
 	/** Inject token authenticator **/
 	public void setgAuth(@Nonnull @NotEmpty final GoogleAuthenticator gAuth) {
 		this.gAuth = gAuth;
 	}
-	
-    @Override
-    protected void doInitialize() throws ComponentInitializationException {
-        super.doInitialize();
-    }
+
+	public void setgAuthIssuerName(@Nonnull @NotEmpty final String gAuthIssuerName) {
+		this.gAuthIssuerName = gAuthIssuerName;
+	}
+
+	@Override
+	protected void doInitialize() throws ComponentInitializationException {
+	    super.doInitialize();
+	}
 
 	@Override
 	protected boolean doPreExecute(ProfileRequestContext profileRequestContext) {
 		log.debug("Entering GenerateNewToken doPreExecute");
-		
+
 		try {
 			tokenCtx = profileRequestContext.getSubcontext(AuthenticationContext.class)
 					.getSubcontext(TokenUserContext.class, true);
@@ -57,44 +66,44 @@ public class GenerateNewToken extends AbstractProfileAction {
 		} catch (Exception e) {
 			log.debug("Error with doPreExecute", e);
 			return false;
-			
+
 		}
-		
-	}	
-	
+
+	}
+
     @Override
 	protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
     	log.debug("Entering GenerateNewToken doExecute");
-			
+
 		try {
 			log.debug("Trying to create new token for {}", upCtx.getUsername());
 			generateToken();
 		} catch (Exception e) {
 			log.debug("Failed to create new token", e);
 		}
-		
+
 	}
 
 	private void generateToken() {
 		log.debug("Generating new token shared secret and URL for {}", upCtx.getUsername());
-		
+
 		try {
 			final GoogleAuthenticatorKey key = gAuth.createCredentials();
-			
-			String totpUrl = GoogleAuthenticatorQRGenerator.getOtpAuthURL("TOTP", upCtx.getUsername(), key);
+
+			String totpUrl = GoogleAuthenticatorQRGenerator.getOtpAuthURL(gAuthIssuerName, upCtx.getUsername(), key);
 			log.debug("Totp URL for {} is {}", upCtx.getUsername(), totpUrl);
 			tokenCtx.setTotpUrl(totpUrl);
 
 			String sharedSecret = StringSupport.trimOrNull(key.getKey());
 			log.debug("Shared secret for {} is {}", upCtx.getUsername(), sharedSecret);
 			tokenCtx.setSharedSecret(sharedSecret);
-			
+
 		} catch (Exception e) {
 			log.debug("Error generating new token",e);
 		}
 
-		
-		
+
+
 	}
 
 }
